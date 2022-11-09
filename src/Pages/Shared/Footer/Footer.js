9fcc2b6b665921e235539511104f453041ef679f
DevMazed04 <mdmazedislam2684@gmail.com>
2022-11-10 02:33:19 +0600

    footer added

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer p-2 fixed-botto text-white text-center'>
+      <h5 className="mt-4">Food For You</h5>
+      <p> <small>Copyright © 2022 - All Rights Reserved.</small></p>
+    </div>
+  );
+};
+
+export default Footer;
