7e8fa7be1b0a0d1d00245308ce5944f75e0a43ef
DevMazed04 <mdmazedislam2684@gmail.com>
2022-11-10 09:25:50 +0600

    create private route for the checkout page

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,14 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Blog from "../../Pages/Blog/Blog";
-// import Checkout from "../../Pages/Checkout/Checkout";
+import Checkout from "../../Pages/Checkout/Checkout";
 import FAQ from "../../Pages/FAQ/FAQ";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Page404 from "../../Pages/Page404/Page404";
 import Register from "../../Pages/Register/Register";
 import Services from "../../Pages/Shared/Services/Services";
-// import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 export const router = createBrowserRouter([
    {
@@ -33,14 +33,6 @@ export const router = createBrowserRouter([
          //    element: <serviceDetails></serviceDetails>,
          //    loader: ({ params }) => fetch(`https://programming-path-server.vercel.app/service-details/${params.id}`)
          // },
-         // {
-         //    path: '/checkout-service/:id',
-         //    // loader: ({ params }) => fetch(`https://programming-path-server.vercel.app/service-details/${params.id}`),
-         //    element:
-         //       <PrivateRoute>
-         //          <Checkout></Checkout>
-         //       </PrivateRoute>
-         // },
          {
             path: '/faq',
             element: <FAQ></FAQ>
@@ -56,6 +48,14 @@ export const router = createBrowserRouter([
          {
             path: '/register',
             element: <Register></Register>
+         },
+         {
+            path: '/checkout/:id',
+            // loader: ({ params }) => fetch(`https://programming-path-server.vercel.app/service-details/${params.id}`),
+            element:
+               <PrivateRoute>
+                  <Checkout></Checkout>
+               </PrivateRoute>
          }
       ]
    },
